be558245f29044669586f67fdd5d7f2bb660de7d
Jakub Mikulas <jakub@snyk.io>
2021-08-17 12:13:37 +0200

    fix: ignore-file generator
    Fix shellcheck errors

--- generator/run.sh
+++ generator/run.sh
@@ -1,4 +1,7 @@
-SCRIPT_DIR=`dirname "$0"`
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
 SOURCE_DIR="./source"
 EMPTY_OUTPUT="../empty.dcignore"
 FULL_OUTPUT="../full.dcignore"
@@ -27,17 +30,17 @@ done > "$FULL_OUTPUT"
 
 echo -e "\n# Hidden directories\n.*/" >> "$FULL_OUTPUT"
 
-while read -r file; do
-	file_name=`basename $file`
+find $SOURCE_DIR -name '*.gitignore' | while read file; do
+	file_name=$(basename "$file")
 	declare -a rules=()
 	while read -r line; do
-		if ! [[ -z "${line// }" ]] && ! [[ "$line" =~ $REGEX ]]; then
+		if [[ -n "${line// }" ]] && ! [[ "$line" =~ $REGEX ]]; then
 			# The line is not empty, nor a comment, nor a rule matching:
 			# a "/.dir_name/" directory, as we already have a catch-all rule above
 			# a "/file_name.ext" file, as we already have extension-based filtering
 			rules+=("$line")
 		fi
-	done <<< `cat "$file"`
+	done < "$file"
 
 	# Debug log for checking number of rules per file.
 	# echo "${file_name%.*} RULES: ${#rules[@]}"
@@ -48,8 +51,8 @@ while read -r file; do
 		   echo "$r" >> $FULL_OUTPUT
 		done
 	fi
-done <<< `find $SOURCE_DIR -name '*.gitignore'`
+done
 
 cd ..
-echo "Parsing completed. Check '`pwd`/`basename $FULL_OUTPUT`' file for results."
+echo "Parsing completed. Check '$(pwd)/$(basename $FULL_OUTPUT)' file for results."
 exit 0
